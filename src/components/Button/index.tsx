863465d4296c3b22900f26109c1b252692bc357d
joao-marco-jf <joaomarcos00512@gmail.com>
2023-12-21 14:24:13 -0300

    Remoção de componente

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-interface ButtonTypes{
-    value: string
-}
-
-export default function Button(props: ButtonTypes){
-    return(
-        <button className="px-[2rem] py-[.8rem] bg-black text-white rounded">{props.value}</button>
-    )
-}
